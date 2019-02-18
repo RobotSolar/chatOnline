7e6cfcf50c9f9ffea27b334e822cee46b5cf159d
RobotSolar <35856556+RobotSolar@users.noreply.github.com>
2019-02-17 19:44:45 -0600

    login auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from "./guards/auth.guard";
-import { NologinGuard } from "./guards/nologin.guard";
+import { NoLoginGuard } from "./guards/no-login.guard";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate : [AuthGuard] },
-  { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule', canActivate : [NologinGuard] },
+  { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule', canActivate : [NoLoginGuard] },
 ];
 
 @NgModule({
